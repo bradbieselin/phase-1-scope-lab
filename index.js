c22766203548d7e29f17b28691852c1920be678d
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-05 09:56:22 -0400

    commit tree fc37e268a5eae84a140a8b503f594ccda38b7e93

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const bestCustomer = "not bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+}
